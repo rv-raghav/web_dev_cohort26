947647ec13b639113746913c4ae152dd9e09564e
Raghav Verma <raghav2103rv@gmail.com>
2026-04-13 01:52:51 +0530

    typescript express setup added

---
+++ 05-backend-engineering/01-intro-to-backend-engineering/node/newindex.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/menu', (req, res) =>
+  res.json({
+    items: ['thali, biryani'],
+  }),
+);
+
+app.post('/order', (req, res) => {
+  res.status(200).json({
+    status: 'received',
+    order: req.body,
+  });
+});
